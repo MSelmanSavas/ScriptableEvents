7e93e076bb1e58d2ddab5c182d41044a24ea0423
MSelmanSavas <selmansavas@agave.games>
2023-06-04 21:42:32 +0300

    Forgot to send int scriptable value

--- Runtime/Variables/Primitives/IntScriptableValue.cs
+++ Runtime/Variables/Primitives/IntScriptableValue.cs
@@ -1,6 +1,7 @@
 using MSS.ScriptableEvents.Variables;
 using UnityEngine;
 
+[System.Serializable]
 [CreateAssetMenu(fileName = "NewIntValue", menuName = "ScriptableEvents/Variables/Create Int Variable")]
 public class IntScriptableValue : GenericScriptableVariable<int>
 {
