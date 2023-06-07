988cf08ad742d0cc177bfa43b181609098f9c5eb
MSelmanSavas <selmansavas@agave.games>
2023-06-08 01:48:18 +0300

    Added created a new event class for generic scriptable event

--- Runtime/Events/Primitives/ScriptablePrimitives/GenericScriptableEvent.cs
+++ Runtime/Events/Primitives/ScriptablePrimitives/GenericScriptableEvent.cs
@@ -3,7 +3,7 @@ namespace MSS.ScriptableEvents.Events
     [System.Serializable]
     public class GenericScriptableEvent<T> : BaseScriptableEvent<T>
     {
-        public GenericEvent<T> GenericEvent;
+        public GenericEvent<T> GenericEvent = new();
         public override IEventLogic<T> EventLogic => GenericEvent;
         public override IEventInvoker<T> EventInvoker => GenericEvent;
         public override IEventData<T> EventData => GenericEvent;
